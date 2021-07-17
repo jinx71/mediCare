8f947f6ed3f5ca2f987cada478997fd2da2aa159
jinx71 <karim.sazed@gmail.com>
2021-07-17 18:57:16 +0000

    feat: create Layout component

---
+++ medicare/client/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = () => (
+  <div className="flex min-h-screen flex-col">
+    <Navbar />
+    <main className="flex-1">
+      <Outlet />
+    </main>
+    <Footer />
+    <ToastContainer position="top-right" autoClose={3000} newestOnTop theme="light" />
+  </div>
+);
+
+export default Layout;
