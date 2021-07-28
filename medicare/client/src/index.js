4ce46e7b04e12931eb31e84a88d9f8b775423112
jinx71 <karim.sazed@gmail.com>
2021-07-28 17:55:28 +0000

    chore: initialize client application entry point

---
+++ medicare/client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import './index.css';
+
+// React 17 API — ReactDOM.render, not createRoot (per the 2021–2022 stack lock).
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
